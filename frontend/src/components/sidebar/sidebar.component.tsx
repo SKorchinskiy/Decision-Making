25780b734f647bc589f0a44d045e32973144b4cf
SKorchinskiy <ddphyk@gmail.com>
2023-12-20 22:59:17 +0200

    + add sidebar component

---
+++ frontend/src/components/sidebar/sidebar.component.tsx
@@ -0,0 +1,10 @@
+import HistoryBoard from "../history-board/history-board.component";
+import "./sidebar.styles.css";
+
+export default function SideBar() {
+  return (
+    <div className="sidebar-container">
+      <HistoryBoard />
+    </div>
+  );
+}
